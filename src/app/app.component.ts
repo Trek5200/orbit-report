ca841b96c6a3704ea45206f0cc78ec3d181cb664
unknown <singletrackgator@gmail.com>
2019-12-17 16:01:25 -0600

    Section 8) completed. Assignment #6 without Bonus Completed. Console.log()'s commented out.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent {
 
         // 8) make a copy of the sourceList to be shown to the user
         this.displayList = this.sourceList.slice(0);
-                  console.log(this.displayList);
+                        // console.log(this.displayList);
 
       }.bind(this));
     }.bind(this));
@@ -50,7 +50,7 @@ export class AppComponent {
     // assign this.displayList to be the the array of matching satellites
     // this will cause Angular to re-make the table, but now only containing matches
     this.displayList = matchingSatellites;
-              console.log(this.displayList);
+                        // console.log(this.displayList);
   }
  
   // constructor() {
